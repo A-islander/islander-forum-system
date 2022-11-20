6b4c8fe833b8c22c2dfdd2a0ec06a4ae3c8fa821
A-islander <aislander0@outlook.com>
2022-11-20 20:18:03 +0800

    update route.go

--- route/route.go
+++ route/route.go
@@ -40,24 +40,6 @@ func Init() *http.ServeMux {
 	}.AddGroup("img").AddMiddleward(calcVisitTimeMiddleware)
 	httpHelper.SetMuxHandle(forumServer, imgHandleArr)
 
-	// mid := totalMiddleware
-	// forumServer.Handle("/plate/get", mid(getForumPlate))
-	// forumServer.Handle("/forum/get", mid(getForumPost))
-	// forumServer.Handle("/forum/index", mid(getForumPostIndex))
-	// forumServer.Handle("/forum/list", mid(getForumPostList))
-	// forumServer.Handle("/forum/indexLast", mid(getForumIndexLastTime))
-	// forumServer.Handle("/forum/userList", mid(getUserForumPostList))
-	// forumServer.Handle("/forum/post", mid(postForumPost))
-	// forumServer.Handle("/forum/reply", mid(replyForumPost))
-	// forumServer.Handle("/forum/sage/add", mid(sageAdd))
-	// forumServer.Handle("/forum/sage/sub", mid(sageSub))
-	// forumServer.Handle("/forum/sage/list", mid(sageList))
-	// forumServer.Handle("/forum/delete/ownPost", mid(deleteOwnPost))
-	// forumServer.Handle("/forum/recover/ownPost", mid(recoverOwnPost))
-	// forumServer.Handle("/img/token", mid(getImgToken))
-	// forumServer.Handle("/img/upload", mid(postImgUpload))
-
-	fmt.Println(forumServer)
 	fmt.Printf("listen to port %s", port)
 	http.ListenAndServe(port, forumServer)
 
