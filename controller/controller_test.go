70588853c1e981c3e5221f24785d725dbc0f82f4
A-islander <aislander0@outlook.com>
2024-02-23 19:04:37 +0800

    add debug

--- controller/controller_test.go
+++ controller/controller_test.go
@@ -7,55 +7,55 @@ import (
 	"time"
 )
 
-// func TestGetForumIndex(t *testing.T) {
-// 	fmt.Println(GetForumPostIndex(1, 0, 10))
+func TestGetForumIndex(t *testing.T) {
+	fmt.Println(GetForumPostIndex(1, 0, 10))
+}
+
+// func TestGetForumPost(t *testing.T) {
+// 	ReplyForumPost("test", 1, nil, 1, "", "name")
+// 	PostForumPost("test", "test", nil, 1, 1, "", "name")
 // }
 
-// // func TestGetForumPost(t *testing.T) {
-// // 	ReplyForumPost("test", 1, nil, 1, "", "name")
-// // 	PostForumPost("test", "test", nil, 1, 1, "", "name")
-// // }
+func TestSage(t *testing.T) {
+	// fmt.Println(SageAdd(1, 3))
+}
 
-// func TestSage(t *testing.T) {
-// 	// fmt.Println(SageAdd(1, 3))
-// }
+func TestForum(t *testing.T) {
+	fmt.Println(GetForumPlate())
+}
 
-// func TestForum(t *testing.T) {
-// 	fmt.Println(GetForumPlate())
-// }
+func TestGetLast(t *testing.T) {
+	fmt.Println(GetForumIndexLastTime(0, 10, []int{}))
+}
 
-// func TestGetLast(t *testing.T) {
-// 	fmt.Println(GetForumIndexLastTime(0, 10, []int{}))
-// }
+func TestDelIntArr(t *testing.T) {
+	arr := delIntArr([]int{1, 2, 3}, 1)
+	fmt.Println(arr)
+}
 
-// func TestDelIntArr(t *testing.T) {
-// 	arr := delIntArr([]int{1, 2, 3}, 1)
-// 	fmt.Println(arr)
-// }
+func TestGetUserArr(t *testing.T) {
+	// fmt.Println(model.GetUserArr([]int{}))
+}
 
-// func TestGetUserArr(t *testing.T) {
-// 	// fmt.Println(model.GetUserArr([]int{}))
-// }
+func TestGetForumPostByUid(t *testing.T) {
+	// fmt.Println(GetForumPostByUid(1, 0, 10))
+}
 
-// func TestGetForumPostByUid(t *testing.T) {
-// 	// fmt.Println(GetForumPostByUid(1, 0, 10))
-// }
+func TestGetImgToken(t *testing.T) {
+	fmt.Println(GetImgToken())
+}
 
-// func TestGetImgToken(t *testing.T) {
-// 	fmt.Println(GetImgToken())
-// }
+func TestChangePost(t *testing.T) {
+	// ChangePostPlate(57, 1)
+}
 
-// func TestChangePost(t *testing.T) {
-// 	// ChangePostPlate(57, 1)
-// }
-
-// // 还有错误没处理
-// func TestStrOperate(t *testing.T) {
-// 	str := `[decide + -]`
-// 	ctx := context.WithValue(context.Background(), "postId", 1)
-// 	newStr := Eval(str, ctx)
-// 	fmt.Println(newStr)
-// }
+// 还有错误没处理
+func TestStrOperate(t *testing.T) {
+	str := `[decide + -]`
+	ctx := context.WithValue(context.Background(), "postId", 1)
+	newStr := Eval(str, ctx)
+	fmt.Println(newStr)
+}
 
 func TestDiscussOperate(t *testing.T) {
 	str := `[[和岛民娘聊会] "123123"]`
